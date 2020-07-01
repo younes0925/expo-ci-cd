3cf19054994d23b9081c7cdd79514384f6eea3bd
Younes Bouchama <younes.bouchama@laposte.fr>
2020-07-01 13:51:57 +0200

    update file

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open App.js to start working on your app!</Text>
+      <Text>Open App.js to start working!</Text>
       <StatusBar style="auto" />
     </View>
   );
